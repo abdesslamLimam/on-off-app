72e34c581375e89851d76aff7b27f598d89e085c
abdesslam.limam <abdesslamlimam@gmail.com>
2020-10-11 23:27:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+var data = require('./data.json')
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded())
+var port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log('Server is running at port: ',port)
+});
+
+app.get('/',(req,res)=>{
+    res.json(data)
+});
+
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    var newdata = JSON.stringify(req.body)
+    fs.writeFileSync('./data.json',newdata,'utf-8')
+    res.end('done')
+})
